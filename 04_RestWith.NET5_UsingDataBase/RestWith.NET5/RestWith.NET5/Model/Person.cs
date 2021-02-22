8e31c6c21448e328ecf940d4f7e289647bcfb179
Josivaldo <josivaldojoao14@gmail.com>
2021-02-21 23:11:42 -0300

    Implementing Database

---
+++ 04_RestWith.NET5_UsingDataBase/RestWith.NET5/RestWith.NET5/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWith.NET5.Model
+{
+    // Indica qual é a tabela referente no BD (no caso, essa classe pertence à tabela "person")
+    [Table("person")]
+    public class Person
+    {
+        // Indica a qual tabela pertence no BD (no BD, o Id está em minusculo)
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+
+        [Column("gender")]
+        public string Gender { get; set; }
+    }
+}
