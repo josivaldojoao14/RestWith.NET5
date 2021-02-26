b875942f1c19c6d9806d42a3e94e1747149c0fb1
Josivaldo <josivaldojoao14@gmail.com>
2021-02-26 12:04:20 -0300

    content negociation using XML format

---
+++ 11_RestWith.NET5_ContentNegociation/RestWith.NET5/RestWith.NET5/Data/VO/PersonVO.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestWith.NET5.Data.VO
+{
+    public class PersonVO
+    {
+        [JsonPropertyName("Código")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("Primeiro nome")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("Último nome")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("Endereço")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("Sexo")]
+        public string Gender { get; set; }
+    }
+}
