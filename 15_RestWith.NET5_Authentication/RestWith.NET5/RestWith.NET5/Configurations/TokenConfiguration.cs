34dcb17e17fbaac62088c6e23f3b66311ac3a101
Josivaldo <josivaldojoao14@gmail.com>
2021-03-14 12:00:31 -0300

    Using authentication

---
+++ 15_RestWith.NET5_Authentication/RestWith.NET5/RestWith.NET5/Configurations/TokenConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace RestWith.NET5.Configurations
+{
+    // É preciso fazer configurações dessas propriedades no 'appsettings.json'
+    public class TokenConfiguration
+    {
+        public string Audience { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Secrete { get; set; }
+
+        public int Minutes { get; set; }
+
+        public int DayToExpire { get; set; }
+    }
+}
--- 15_RestWith.NET5_Authentication/RestWith.NET5/RestWith.NET5/Repository/UserRepository.cs
+++ 15_RestWith.NET5_Authentication/RestWith.NET5/RestWith.NET5/Repository/UserRepository.cs
@@ -25,6 +25,31 @@ namespace RestWith.NET5.Repository
             return _context.Users.FirstOrDefault(u => (u.Username == user.UserName) && (u.Password == pass));
         }
 
+        // Método responsável por atualizar as informações dos clientes
+        public User RefreshUserInfo(User user)
+        {
+            // Se não for encontrado ninguém no BD com o mesmo ID recebido do 'user' do param, ele retorna nulo
+            if (!_context.Users.Any(u => u.Id.Equals(user.Id))) return null;
+
+            // Se for encontrado alguém no BD que tenha o mesmo ID que o 'user' do param, ele armazena isso em 'result' 
+            var result = _context.Users.SingleOrDefault(p => p.Id == user.Id);
+            if (result != null)
+            {
+                // Se o 'result' for diferente de nulo, ele vai tentar atualizar as infos do usuario
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return result;
+        }
+
         // Método responsável por encriptar a senha
         private string ComputeHash(string input, SHA256CryptoServiceProvider Algorithm)
         {
